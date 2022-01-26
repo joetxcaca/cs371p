95c0f367949e7bd5288bf6c5866e2931c3653a89
Glenn Downing <downing@cs.utexas.edu>
2022-01-26 14:03:23 -0600

    another commit

--- notes/01-26.cpp
+++ notes/01-26.cpp
@@ -48,7 +48,7 @@ Perusall, collaborative annotation tool (you must go through the Canvas assignme
 	the first one will be the syllabus
 
 Zoom
-	5.9.1
+	5.9.3
 	must use UT EID credentials (@eid.utexas.edu)
 	classes recorded
 	published later that day
@@ -76,3 +76,183 @@ specifications grading
 very much a C++ class
 */
 
+/*
+Paper #02: Makefile
+remember to go to Perusall THROUGH Canvas
+*/
+
+/*
+Project #1: Collatz
+*/
+
+cout << "hi" << 2 << 3.4 << endl;
+
+(cout << "hi") << 2
+(cout << 2) << 3.4
+
+// sample input
+
+1 10
+100 200
+201 210
+900 1000
+
+// sample output
+
+1 10 20
+100 200 125
+201 210 89
+900 1000 174
+
+/*
+read a pair of ints
+compute an answer
+print the original pair and the answer
+
+compute the cycle length on all of the numbers in the range specified by the pair
+compute the max cycle length
+*/
+
+/*
+read, eval, print (REPL)
+*/
+
+// sample output (alternative)
+
+4
+1 10 20
+100 200 125
+201 210 89
+900 1000 174
+
+// sample output (alternative)
+
+1 10 20
+100 200 125
+201 210 89
+900 1000 174
+<sentinel>
+
+/*
+three styles of describing input
+	1. just the input, expecting read to eventually fail
+	2. specify the number of test cases beforehand
+	3. use a sentinel at the end
+*/
+
+/*
+the skeleton has three parts
+	1. Collatz.hpp / Collatz.cpp; the kernel, where the computation happens
+	2. TestCollatz.cpp;           test harness, which will use Google Test, a unit test framework
+	3. RunCollatz.cpp;            run harness, which will the program they way HackerRank wants
+*/
+
+/*
+you need to MERGE Collatz.cpp and RunCollatz.cpp -> MyHackerRank.cpp
+*/
+
+// ---------------
+// TestCollatz.cpp
+// ---------------
+
+// https://code.google.com/p/googletest/wiki/V1_7_Primer#Basic_Assertions
+
+// --------
+// includes
+// --------
+
+#include "gtest/gtest.h"
+
+#include "Collatz.hpp" // the kernel
+
+using namespace std;
+
+// ----------------
+// max_cycle_length
+// ----------------
+
+TEST(CollatzFixture, max_cycle_length_0) {
+    ASSERT_EQ(max_cycle_length(1, 10), 20u);}
+
+TEST(CollatzFixture, max_cycle_length_1) {
+    ASSERT_EQ(max_cycle_length(100, 200), 125u);}
+
+TEST(CollatzFixture, max_cycle_length_2) {
+    ASSERT_EQ(max_cycle_length(201, 210), 89u);}
+
+TEST(CollatzFixture, max_cycle_length_3) {
+    ASSERT_EQ(max_cycle_length(900, 1000), 174u);}
+
+// --------------
+// RunCollatz.cpp
+// --------------
+
+// --------
+// includes
+// --------
+
+#include <iostream> // cin, cout, endl
+#include <sstream>  // istringstream
+#include <string>   // getline, string
+
+#include "Collatz.hpp" // the kernel, max_cycle_length
+
+using namespace std;
+
+// ----
+// main
+// ----
+
+int main () {
+    string s;
+    while (getline(cin, s)) { // what is getline() return type? istream, cin
+    					 	  // there must be a conversion from istream -> bool
+        // ----
+        // read
+        // ----
+
+        istringstream iss(s); // istringstream acts like, both, an istream and a string
+        unsigned i, j;
+        iss >> i >> j;        // iss is acting like an istream
+
+        // ----
+        // eval
+        // ----
+
+        unsigned v = max_cycle_length(i, j);
+
+        // -----
+        // print
+        // -----
+        cout << i << " " << j << " " << v << endl;}
+
+    return 0;}
+
+
+// -----------
+// Collatz.cpp
+// -----------
+
+// --------
+// includes
+// --------
+
+#include <cassert> // assert
+
+#include "Collatz.hpp"
+
+using namespace std;
+
+// ----------------
+// max_cycle_length
+// ----------------
+
+unsigned max_cycle_length (unsigned i, unsigned j) {
+    assert(i > 0);
+    assert(j > 0);
+
+    // <your code>
+    unsigned v = i + j; // replace!
+
+    assert(v > 0);
+    return v;}
