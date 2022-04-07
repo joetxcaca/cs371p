eac846c5de0bcfccc579eb84db67d5eca8d4f9ad
Glenn Downing <downing@cs.utexas.edu>
2022-04-07 18:55:30 -0500

    another commit

--- notes/04-08.cpp
+++ notes/04-08.cpp
@@ -3,86 +3,7 @@
 // -----------
 
 /*
-Class
-	MWF 1-2 pm CT
-
-Website
-	https://www.cs.utexas.edu/users/downing/cs371p/Schedule.html
-
-Help Sessions
-	W 5-7 pm
-
-Office Hours
-	Glenn Downing
-		Th 3-4 pm, Zoom
-
-	Philo Lin (undergrad, '23)
-		M 2-3 pm, Zoom
-
-	Serdjan Rolovic (grad, '22)
-		Th 2-3 pm, Zoom
-
-Cold Calling
-	you're only called ONCE per rotation
-	it's totally fine to not know the answer, the idea is to discuss and to learn
-
-Notes
-	these notes will be posted on GitLab
-
-Canvas
-	personal questions
-	please message all 3 of us
-
-Ed Discussion (EdStem), replaces Piazza
-	class questions
-	ask and answer questions
-	please be proactive
-
-GitLab, like GitHub, do a git clone, later git pull, on	 a regular basis
-	notes
-	examples
-	exercises
-
-HackerRank, competitive programming platform
-	exercises
-
-Perusall, collaborative annotation tool (you must go through the Canvas assignment)
-	annotate a series of papers
-	the first one will be the syllabus
-
-Zoom
-	5.9.6
-	must use UT EID credentials (@eid.utexas.edu)
-	classes recorded
-	published later that day
-
-Specifications Grading
-	please ask about this if you don't undestand it
-	please use the Google Sheet that we're providing to track your grades
-
-Discord
-	https://discord.gg/Qd3DVQZG
-
-Grades Tracker Google Sheet
-	https://docs.google.com/spreadsheets/d/1nt_FXi5vYuZlsxTPnanItjcivtZy8Gu4AADpPsdBWhM/
-
-Virtual Token Google Form
-	https://forms.gle/yoiKV2Vw1cSWbxBQ7/
-*/
-
-/*
-cold calling
-specifications grading
-very much a C++ class
-*/
-
-/*
-please track your grades
-please be responsive on Canvas
-*/
-
-/*
-Paper #10: Why getter and setter methods are evil
+Paper #11: More getters and setters
 remember to go to Perusall THROUGH Canvas
 */
 
@@ -123,6 +44,7 @@ Takeaways:
 */
 
 /*
+getters and setters
 friend
 map
 */
