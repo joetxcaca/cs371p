5f6fed65f0793e10ecd4837a605bb3c24ed7dc13
Glenn Downing <downing@cs.utexas.edu>
2022-04-11 14:04:26 -0500

    another commit

---
+++ cpp/Initializations.cpp
@@ -0,0 +1,241 @@
+// -------------------
+// Initializations.cpp
+// -------------------
+
+// http://en.cppreference.com/w/cpp/language/value_initialization
+// http://en.cppreference.com/w/cpp/language/initializer_list
+// http://en.cppreference.com/w/cpp/utility/initializer_list
+
+#include <cassert>          // assert
+#include <initializer_list> // initializer_list
+#include <iostream>         // cout, endl
+#include <vector>           // vector
+
+using namespace std;
+
+void test1 () {
+    int i(2);
+    assert(i == 2);
+
+    int j = 2;
+    assert(j == 2);
+
+    int k{2};
+    assert(k == 2);
+
+    int l = {2};
+    assert(l == 2);}
+
+
+
+void test2 () {
+    int i(2.0);
+    assert(i == 2);
+
+    int j = 2.0;
+    assert(j == 2);
+
+//  int k{2.0};     // error: narrowing conversion of '2.0e+0' from 'double' to 'int'
+
+//  int l = {2.0};  // error: narrowing conversion of '2.0e+0' from 'double' to 'int'
+    }
+
+
+
+struct A {
+    int i;
+    int j;
+
+    A             ()         = default;   // default constructor
+    A             (const A&) = default;   // copy constructor
+    A& operator = (const A&) = default;   // copy assignment operator
+    ~A            ()         = default;}; // destructor
+
+void test3 () {
+//  A x(2, 3);    // error: no matching function for call to 'A::A(int, int)'
+
+    A z{2, 3};
+    assert(&z);
+
+    A t = {2, 3};
+    assert(&t);}
+
+
+
+struct B {
+    static int c;
+
+    int i;
+    int j;
+
+    B             ()         = default; // default constructor
+    B             (const B&) = default; // copy constructor
+    B& operator = (const B&) = default; // copy assignment operator
+    ~B            ()         = default; // destructor
+
+    B (int, int) {
+        ++c;}};
+
+int B::c = 0;
+
+void test4 () {
+    B x(2, 3);          // B(int, int)
+    assert(B::c == 1);
+
+    B z{2, 3};          // B(int, int)
+    assert(B::c == 2);
+
+    B t = {2, 3};       // B(int, int)
+    assert(B::c == 3);}
+
+
+
+struct C {
+    static int c;
+
+    int i;
+    int j;
+
+    C             ()         = default; // default constructor
+    C             (const C&) = default; // copy constructor
+    C& operator = (const C&) = default; // copy assignment operator
+    ~C            ()         = default; // destructor
+
+    explicit C (int, int) {
+        ++c;}};
+
+int C::c = 0;
+
+void test5 () {
+    C x(2, 3);         // C(int, int)
+    assert(C::c == 1);
+
+    C z{2, 3};         // C(int, int)
+    assert(C::c == 2);
+
+//  C t = {2, 3};      // error: converting to 'C' from initializer list would use explicit constructor 'C::C(int, int)'
+    }
+
+
+
+struct D {
+    static int c;
+
+    int i;
+    int j;
+
+    D             ()         = default; // default constructor
+    D             (const D&) = default; // copy constructor
+    D& operator = (const D&) = default; // copy assignment operator
+    ~D            ()         = default; // destructor
+
+    D (initializer_list<int>) {
+        ++c;}};
+
+int D::c = 0;
+
+void test6 () {
+//  D x(2, 3);          // error: no matching function for call to 'D::D(int)'
+
+    D z{2, 3};          // D(initializer_list<int>)
+    assert(D::c == 1);
+
+    D t = {2, 3};       // D(initializer_list<int>)
+    assert(D::c == 2);}
+
+
+
+struct E {
+    static int c;
+
+    int i;
+    int j;
+
+    E             ()         = default; // default constructor
+    E             (const E&) = default; // copy constructor
+    E& operator = (const E&) = default; // copy assignment operator
+    ~E            ()         = default; // destructor
+
+    explicit E (initializer_list<int>) {
+        ++c;}};
+
+int E::c = 0;
+
+void test7 () {
+//  E x(2, 3);    // error: no matching function for call to 'D::D(int)'
+
+    E z{2, 3};    // E(initializer_list<int>)
+    assert(&z);
+
+//  E t = {2, 3}; // error: converting to 'E' from initializer list would use explicit constructor 'E::E(std::initializer_list<int>)'
+    }
+
+
+
+struct F {
+    static int c;
+
+    int i;
+    int j;
+
+    F             ()         = default; // default constructor
+    F             (const F&) = default; // copy constructor
+    F& operator = (const F&) = default; // copy assignment operator
+    ~F            ()          = default; // destructor
+
+    F (int) {
+        c += 2;}
+
+    F (initializer_list<int>) {
+        c += 3;}
+
+    bool operator == (const F&) {
+    	return true;}};
+
+int F::c = 0;
+
+void test8 () {
+    F x(2);              // F(int)
+    assert(F::c == 2);
+
+    F y = 2;             // F(int)
+    assert(F::c == 4);
+
+    F z{2};              // F(initializer_list<int>)
+    assert(F::c == 7);
+
+    F t = {2};           // F(initializer_list<int>)
+    assert(F::c == 10);}
+
+
+
+void test9 () {
+    vector<int> x(2);                               // vector<int>(int)
+    assert(x.size() == 2);
+    assert(equal(begin(x), end(x), begin({0, 0})));
+
+//  vector<int> y = 2;                              // error: conversion from 'int' to non-scalar type 'std::vector<int>' requested
+
+    vector<int> z{2};                               // vector<int>(initializer_list<int>)
+    assert(z.size() == 1);
+    assert(equal(begin(z), end(z), begin({2})));
+
+    vector<int> t = {2};                            // vector<int>(initializer_list<int>)
+    assert(t.size() == 1);
+    assert(equal(begin(t), end(t), begin({2})));}
+
+
+
+int main () {
+    cout << "Initializations.cpp" << endl;
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+    test8();
+    test9();
+    cout << "Done." << endl;
+    return 0;}
--- notes/04-11.cpp
+++ notes/04-11.cpp
@@ -50,3 +50,370 @@ iteration
 initializations
 */
 
+
+/*
+set is like Java's TreeSet
+	keys
+	ordered
+	keys must be comparable (<)
+	unique keys
+	find(), only 1 version, read-only
+*/
+
+/*
+unorderedset is like Java's HashSet
+	keys
+	unordered
+	keys must be hashable
+	unique keys
+	find(), only 1 version, read-only
+*/
+
+
+
+
+/*
+map is like Java's TreeMap
+	key, value pairs
+	ordered keys
+	keys must be comparable
+	unique keys
+	find(), two versions, read-only and read-write (values only)
+*/
+
+/*
+unorderedmap is like Java's HashMap
+	key, value pairs
+	unordered
+	keys must be hashable
+	unique keys
+	find(), two versions, read-only and read-write (values only)
+*/
+
+
+
+void test1 () {
+    int i = 0;
+    int s = 0;
+    while (i != 10) {
+        s += i;
+        ++i;}
+    assert(i == 10);
+    assert(s == 45);}
+
+void test2 () {
+    int i = 0;
+    int s = 0;
+    do { // body guaranteed to run at least once
+        s += i;
+        ++i;}
+    while (i != 10);
+    assert(i == 10);
+    assert(s == 45);}
+
+void test3 () {
+    int s = 0;
+    for (int i = 0; i != 10; ++i)
+        s += i;
+//  assert(i == 10);              // error: name lookup of 'i' changed for new ISO 'for' scoping
+    assert(s == 45);}
+
+{
+int i = 0;
+while (i != 10) {
+	s += i;
+	++i;}
+}
+
+void test4 () {
+    const int a[] = {2, 3, 4};
+    const int b[] = {5, 6, 7};
+    int s = 0;
+    for (int i = 0, j = 0; i != 3; ++i, ++j) // all loop variables have to be the same type
+        s += a[i] + b[j];
+    assert(s == 27);}
+
+void test5 () {
+    int a[] = {2, 3, 4};
+    int s   = 0;
+    for (int v : a) // for in, for each
+        s += v;
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({2, 3, 4})));}
+
+// what are the requirements on a
+
+{
+int* b = begin(a);
+int* e = end(a);
+while (b != e) { // using b, e as input iterators
+	int v = *b;
+	s += v;
+	++b;}
+}
+
+void test6 () {
+    int a[] = {2, 3, 4};
+    int s   = 0;
+    for (int v : a) {
+        s += v;
+        ++v;}                                   // ?
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({2, 3, 4})));}
+
+void test7 () {
+    int  a[] = {2, 3, 4};
+    int  s   = 0;
+    int* b   = begin(a);
+    int* e   = end(a);
+    while (b != e) {
+        int v = *b;
+        s += v;
+        ++v;                                    // ?
+        ++b;}
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({2, 3, 4})));}
+
+void test8 () {
+    int a[] = {2, 3, 4};
+    int s   = 0;
+    for (auto v : a) {
+        s += v;
+        ++v;}                                   // ?
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({2, 3, 4})));
+    auto [vx, vy, vz] = a;
+    assert(vx  == 2);
+    assert(vy  == 3);
+    assert(vz  == 4);
+    assert(&vx != &a[0]);}
+
+void test9 () {
+    int a[] = {2, 3, 4};
+    int s   = 0;
+    for (int& r : a) {
+        s += r;
+        ++r;}
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({3, 4, 5})));}
+
+void test10 () {
+    int a[] = {2, 3, 4};
+    int s   = 0;
+    for (auto& r : a) {
+        s += r;
+        ++r;}
+    assert(s == 9);
+    assert(equal(a, a + 3, begin({3, 4, 5})));
+    auto& [rx, ry, rz] = a;
+    assert(rx  == 3);
+    assert(ry  == 4);
+    assert(rz  == 5);
+    assert(&rx == &a[0]);}
+
+void test11 () {
+    list<int> x = {2, 3, 4};
+    int       s = 0;
+    for (int v : x)
+        s += v;
+    assert(s == 9);
+    assert(equal(begin(x), end(x), begin({2, 3, 4})));}
+
+void test12 () {
+    list<int>           x = {2, 3, 4};
+    int                 s = 0;
+    list<int>::iterator b = begin(x);           // x.begin()
+    list<int>::iterator e = end(x);             // x.end()
+    while (b != e) {
+        list<int>::iterator::value_type v = *b;
+        s += v;
+        ++b;}
+    assert(s == 9);
+    assert(equal(begin(x), end(x), begin({2, 3, 4})));}
+
+void test13 () {
+    set<int> x = {2, 3, 4};
+    int      s = 0;
+    for (int v : x)
+        s += v;
+    assert(s == 9);
+    assert(equal(begin(x), end(x), begin({2, 3, 4})));}
+
+void test14 () {
+    set<int>           x = {2, 3, 4};
+    int                s = 0;
+    set<int>::iterator b = begin(x);           // x.begin()
+    set<int>::iterator e = end(x);             // x.end()
+    while (b != e) {
+        set<int>::iterator::value_type v = *b;
+        s += v;
+        ++b;}
+    assert(s == 9);
+    assert(equal(begin(x), end(x), begin({2, 3, 4})));}
+
+void test15 () {
+    set<int> x = {2, 3, 4};
+    int      s = 0;
+//  for (      int& r : x)                              // error: binding reference of type 'int&' to 'const int' discards qualifiers
+    for (const int& r : x)
+        s += r;
+    assert(s == 9);
+    assert(equal(begin(x), end(x), begin({2, 3, 4})));}
+
+void test16 () {
+    map<char, int> x = {{'a', 2}, {'b', 3}, {'c', 4}};
+    int            s = 0;
+    for (pair<char, int> v : x)
+        s += v.second;
+    assert(s == 9);}
+
+void test17 () {
+    map<char, int>           x = {{'a', 2}, {'b', 3}, {'c', 4}};
+    int                      s = 0;
+    map<char, int>::iterator b = begin(x);                       // x.begin()
+    map<char, int>::iterator e = end(x);                         // x.end()
+    while (b != e) {
+        map<char, int>::iterator::value_type v = *b;
+        s += v.second;
+        ++b;}
+    assert(s == 9);}
+
+void test18 () {
+    map<char, int> x = {{'a', 2}, {'b', 3}, {'c', 4}};
+    int            s = 0;
+//  for (pair<      char, int>& p : x)                 // error: non-const lvalue reference to type 'pair<char, [...]>' cannot bind to a value of unrelated type 'const pair<__key_type, [...]>'
+    for (pair<const char, int>& p : x)
+        s += p.second;
+    assert(s == 9);}
+
+void test19 () {
+    ostringstream     out;
+    const vector<int> x = {2, 3, 4};
+    for (size_t i = 0; i != x.size(); ++i)
+        out << x[i] << " \n"[i == (x.size() - 1)];
+    assert(out.str() == "2 3 4\n");}
+
+
+
+
+
+
+// -------------------
+// Initializations.cpp
+// -------------------
+
+// http://en.cppreference.com/w/cpp/language/value_initialization
+// http://en.cppreference.com/w/cpp/language/initializer_list
+// http://en.cppreference.com/w/cpp/utility/initializer_list
+
+#include <cassert>          // assert
+#include <initializer_list> // initializer_list
+#include <iostream>         // cout, endl
+#include <vector>           // vector
+
+using namespace std;
+
+void test1 () {
+    int i(2);
+    assert(i == 2);
+
+    int j = 2;
+    assert(j == 2);
+
+    int k{2};
+    assert(k == 2);
+
+    int l = {2};
+    assert(l == 2);}
+
+
+
+void test2 () {
+    int i(2.0);
+    assert(i == 2);
+
+    int j = 2.0;
+    assert(j == 2);
+
+//  int k{2.0};     // error: narrowing conversion of '2.0e+0' from 'double' to 'int'
+
+//  int l = {2.0};  // error: narrowing conversion of '2.0e+0' from 'double' to 'int'
+    }
+
+
+
+struct A { // a C struct as compared to a C++ one
+    int i;
+    int j;
+
+    A             ()         = default;   // default constructor
+    A             (const A&) = default;   // copy constructor
+    A& operator = (const A&) = default;   // copy assignment operator
+    ~A            ()         = default;}; // destructor
+
+void test3 () {
+//  A x(2, 3);    // error: no matching function for call to 'A::A(int, int)'
+
+    A z{2, 3};    // analogous to initializing an array
+    assert(&z);
+
+    A t = {2, 3};
+    assert(&t);}
+
+
+int a[] = {2, 3, 4};
+
+
+struct B { // C++ struct
+    static int c;
+
+    int i;
+    int j;
+
+    B             ()         = default; // default constructor
+    B             (const B&) = default; // copy constructor
+    B& operator = (const B&) = default; // copy assignment operator
+    ~B            ()         = default; // destructor
+
+    B (int, int) {
+        ++c;}};
+
+int B::c = 0;
+
+void test4 () {
+    B x(2, 3);          // B(int, int)
+    assert(B::c == 1);
+
+    B z{2, 3};          // B(int, int)
+    assert(B::c == 2);
+
+    B t = {2, 3};       // B(int, int)
+    assert(B::c == 3);}
+
+
+
+struct C {
+    static int c;
+
+    int i;
+    int j;
+
+    C             ()         = default; // default constructor
+    C             (const C&) = default; // copy constructor
+    C& operator = (const C&) = default; // copy assignment operator
+    ~C            ()         = default; // destructor
+
+    explicit C (int, int) {
+        ++c;}};
+
+int C::c = 0;
+
+void test5 () {
+    C x(2, 3);         // C(int, int)
+    assert(C::c == 1);
+
+    C z{2, 3};         // C(int, int)
+    assert(C::c == 2);
+
+//  C t = {2, 3};      // error: converting to 'C' from initializer list would use explicit constructor 'C::C(int, int)'
+    }
