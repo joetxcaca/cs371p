485c16c663794f5eb2e1d91ffcaebe1dfe0743d8
Glenn Downing <downing@cs.utexas.edu>
2022-04-22 13:58:40 -0500

    another commit

--- notes/04-22.cpp
+++ notes/04-22.cpp
@@ -45,7 +45,324 @@ Takeaways:
 */
 
 /*
+Vector3
+Life
 Shapes
 Circles
 */
 
+template <typename T>
+class my_vector {
+    friend bool operator == (const my_vector& lhs, const my_vector& rhs) {
+        return (lhs.size() == rhs.size()) && equal(lhs.begin(), lhs.end(), rhs.begin());}
+
+    private:
+        T* _b = nullptr;
+        T* _e = nullptr;
+
+    public:
+        my_vector () = default;
+
+        explicit my_vector (size_t s) :
+                my_vector(s, T())
+            {}
+
+        my_vector (size_t s, const T& v) {
+            if (s != 0) {
+                _b = new T[s];
+                _e = _b + s;
+                fill(begin(), end(), v);}}
+
+        my_vector (initializer_list<T> rhs) {
+            if (rhs.size() != 0) {
+                _b = new T[rhs.size()];
+                _e = _b + rhs.size();
+                copy(rhs.begin(), rhs.end(), begin());}}
+
+        my_vector (const my_vector& rhs) {
+            if (rhs.size() != 0) {
+                _b = new T[rhs.size()];
+                _e = _b + rhs.size();
+                copy(rhs.begin(), rhs.end(), begin());}}
+
+        my_vector (my_vector&& rhs) {
+            swap(rhs);}
+
+        my_vector& operator = (const my_vector& rhs) {
+            if (this == &rhs)
+                return *this;
+            my_vector that(rhs); // copy constructor
+            swap(that);
+            return *this;}
+
+        my_vector& operator = (my_vector&& rhs) {
+            if (this == &rhs)
+                return *this;
+            my_vector that(move(rhs)); // move constructor
+            swap(that);
+            return *this;}
+
+        ~my_vector () {
+            delete [] _b;}
+
+        T& operator [] (size_t i) {
+            assert(i < size());
+            return _b[i];}
+
+        const T& operator [] (size_t i) const {
+            return (*const_cast<my_vector*>(this))[i];}
+
+        T* begin () {
+            return _b;}
+
+        const T* begin () const {
+            return const_cast<my_vector*>(this)->begin();}
+
+        T* end () {
+            return _e;}
+
+        const T* end () const {
+            return const_cast<my_vector*>(this)->end();}
+
+        size_t size () const {
+            return _e - _b;}
+
+        void swap (my_vector& rhs) {
+            std::swap(_b, rhs._b);
+            std::swap(_e, rhs._e);}};
+
+/*
+Project #5: Life
+*/
+
+/*
+John Conway's Game of Life
+*/
+
+/*
+cell has two states
+	alive (*)
+	dead  (.)
+*/
+
+.....
+..*..
+..*..
+..*..
+.....
+
+/*
+5x5, 25 cells
+3  alive
+22 dead
+*/
+
+/*
+neighborhood is defined as all of the adjacent cells
+1. corner:   3
+2. edge:     5
+3. interior: 8
+*/
+
+/*
+if the cell is alive and there are 2 or 3 live neighbors
+	cell stays alive
+
+if the cell is dead and there are 3 live neighbors
+	cell becomes alive
+*/
+
+.....
+.....
+.***.
+.....
+.....
+
+
+.....
+..*..
+..*..
+..*..
+.....
+
+
+// Life.in.txt
+
+5
+
+4 4
+4
+1 1
+1 2
+2 1
+2 2
+1 1
+
+5 5
+3
+2 1
+2 2
+2 3
+2 1
+
+5 5
+4
+1 2
+2 1
+2 3
+3 2
+1 1
+
+5 6
+6
+1 2
+1 3
+2 1
+2 4
+3 2
+3 3
+1 1
+
+6 6
+5
+1 2
+2 3
+3 1
+3 2
+3 3
+4 1
+
+
+
+*** Life<ConwayCell> 4x4 ***
+
+Generation = 0, Population = 4.
+....
+.**.
+.**.
+....
+
+Generation = 1, Population = 4.
+....
+.**.
+.**.
+....
+
+*** Life<ConwayCell> 5x5 ***
+
+Generation = 0, Population = 3.
+.....
+.....
+.***.
+.....
+.....
+
+Generation = 1, Population = 3.
+.....
+..*..
+..*..
+..*..
+.....
+
+Generation = 2, Population = 3.
+.....
+.....
+.***.
+.....
+.....
+
+*** Life<ConwayCell> 5x5 ***
+
+Generation = 0, Population = 4.
+.....
+..*..
+.*.*.
+..*..
+.....
+
+Generation = 1, Population = 4.
+.....
+..*..
+.*.*.
+..*..
+.....
+
+*** Life<ConwayCell> 5x6 ***
+
+Generation = 0, Population = 6.
+......
+..**..
+.*..*.
+..**..
+......
+
+Generation = 1, Population = 6.
+......
+..**..
+.*..*.
+..**..
+......
+
+*** Life<ConwayCell> 6x6 ***
+
+Generation = 0, Population = 5.
+......
+..*...
+...*..
+.***..
+......
+......
+
+Generation = 1, Population = 5.
+......
+......
+.*.*..
+..**..
+..*...
+......
+
+Generation = 2, Population = 5.
+......
+......
+...*..
+.*.*..
+..**..
+......
+
+Generation = 3, Population = 5.
+......
+......
+..*...
+...**.
+..**..
+......
+
+Generation = 4, Population = 5.
+......
+......
+...*..
+....*.
+..***.
+......
+
+class AbstractCell {
+	...}
+
+class ConwayCell : public AbstractCell {
+	...}
+
+class FredkinCell : public AbstractCell {
+	...}
+
+template <typename C>
+class Life {
+	...}
+
+Life<ConwayCell>  x(5, 5, ConwayCell(...));       // homogeneous grid of ConwayCell
+Life<FredkinCell> y(5, 5, FredkinCell(...));      // homogeneous grid of FredkinCell
+
+Life<AbstractCell> z(5, 5, ConwayCell(...));      // no
+
+Life<AbstractCell*> z(5, 5, ConwayCell(...));     // no
+
+Life<AbstractCell*> z(5, 5, new ConwayCell(...)); // no
