e8a41a7f1d867898c03fcc186fdbc2e5e12a75c8
Glenn Downing <downing@cs.utexas.edu>
2022-04-27 16:15:29 -0500

    another commit

--- notes/04-27.cpp
+++ notes/04-27.cpp
@@ -44,3 +44,199 @@ Shapes
 Circles
 */
 
+
+
+class Shape {
+    friend bool operator == (const Shape& lhs, const Shape& rhs) {
+        return (lhs._x == rhs._x) && (lhs._y == rhs._y);}
+
+    friend istream& operator >> (istream& lhs, Shape& rhs) {
+        return lhs >> rhs._x >> rhs._y;}
+
+    friend ostream& operator << (ostream& lhs, const Shape& rhs) {
+        return lhs << rhs._x << " " << rhs._y;}
+
+    private:
+        int _x;
+        int _y;
+
+    public:
+        Shape (int x, int y) :
+                _x (x),
+                _y (y)
+            {}
+
+        Shape (const Shape&) = default;
+/*
+        Shape (const Shape& rhs) :
+                _x (rhs._x),
+                _y (rhs._y)
+            {}
+*/
+        ~Shape () = default;
+/*
+        ~Shape ()
+            {}
+*/
+        Shape& operator = (const Shape&) = default;
+/*
+        Shape& operator = (const Shape& rhs) {
+            _x = rhs._x;
+            _y = rhs._y;
+            return *this;}
+*/
+        double area () const { // in both
+            return 0;}
+
+        void move (int x, int y) { // in the parent, not child
+            _x = x;
+            _y = y;}};
+
+
+#ifdef TEST0
+void test0 () {
+    Shape x(2, 3);
+    x.move(4, 5);
+    assert(x.area() == 0);
+//  x.radius();            // doesn't compile
+    }
+#endif
+
+#ifdef TEST1
+void test1 () {
+    const Shape x(2, 3);
+          Shape y(4, 5);
+    assert(x != y);
+    y = x;
+    assert(x == y);}
+#endif
+
+#ifdef TEST2
+void test2 () {
+    istringstream sin("4 5");
+    Shape x(2, 3);
+    Shape y(4, 5);
+    sin >> x;
+    assert(x == y);}
+#endif
+
+#ifdef TEST3
+void test3 () {
+    ostringstream sout;
+    Shape x(2, 3);
+    sout << x;
+    assert(sout.str() == "2 3");}
+#endif
+
+
+class Circle : public Shape {
+	friend ==
+		/* need to call Shape's ==
+		   if it were a method
+		   Shape::operator==()
+		   it is NOT a method
+		*/
+		// I need a cast, called static_cast<>, to be able to call the parent's version
+
+	private:
+		int _r;
+
+	public:
+		Circle (int x, int y, int r) :
+			// obligated to call the parent constructor
+			_x (x), // no
+			_r (r)
+			{}
+
+		// don't need move
+		// do need area
+		// do need radius
+
+		Circle& operator = (const Circle&) = default;
+/*
+		Circle& operator = (const Circle& rhs) {
+			Shape::operator=(rhs);
+			_r = rhs._r;
+			return *this;
+*/
+
+6983
+
+#ifdef TEST4
+void test4 () {
+    Circle x(2, 3, 4); // S(), C()
+    x.move(5, 6);
+    assert(x.area()   == 3.14 * 4 * 4);
+    assert(x.radius() == 4);}
+#endif
+
+#ifdef TEST5
+void test5 () {
+    const Circle x(2, 3, 4);
+          Circle y(2, 3, 5);
+    assert(x != y);
+    y = x;
+    assert(x == y);}
+#endif
+
+#ifdef TEST6
+void test6 () {
+    istringstream sin("4 5 6");
+    Circle x(2, 3, 4);
+    Circle y(4, 5, 6);
+    sin >> x;
+    assert(x == y);}
+#endif
+
+#ifdef TEST7
+void test7 () {
+    ostringstream sout;
+    Circle x(2, 3, 4);
+    sout << x;
+    assert(sout.str() == "2 3 4");}
+#endif
+
+
+class Circle : public Shape {
+    friend bool operator == (const Circle& lhs, const Circle& rhs) {
+        return (static_cast<const Shape&>(lhs) == rhs) && (lhs._r == rhs._r);}
+
+    friend istream& operator >> (istream& lhs, Circle& rhs) {
+        return lhs >> static_cast<Shape&>(rhs) >> rhs._r;}
+
+    friend ostream& operator << (ostream& lhs, const Circle& rhs) {
+        return lhs << static_cast<const Shape&>(rhs) << " " << rhs._r;}
+
+    private:
+        int _r;
+
+    public:
+        Circle (int x, int y, int r) :
+                Shape (x, y),
+                _r (r)
+            {}
+
+        Circle (const Circle&) = default;
+/*
+        Circle (const Circle& rhs) :
+                Shape (rhs),
+                _r    (rhs._r)
+            {}
+*/
+        ~Circle () = default;
+/*
+        ~Circle ()
+            {}
+*/
+        Circle& operator = (const Circle&) = default;
+/*
+        Circle& operator = (const Circle& rhs) {
+            Shape::operator=(rhs);
+            _r = rhs._r;
+            return *this;}
+*/
+        double area () const {
+            return 3.14 * _r * _r;}
+
+        int radius () const {
+            return _r;}};
