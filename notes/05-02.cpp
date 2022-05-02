b42808028ec069eecd9c734e7cf863bae93dfbc0
Glenn Downing <downing@cs.utexas.edu>
2022-05-02 14:15:02 -0500

    another commit

--- notes/05-02.cpp
+++ notes/05-02.cpp
@@ -43,3 +43,77 @@ Takeaways:
 Shapes2
 */
 
+/*
+compile time casts
+	const_cast
+	reinterprest_cast
+	static_cast
+
+run time casts
+	dynamic_cast
+*/
+
+#ifdef TEST8
+void test8 () {
+//  Circle* const p = new Shape(2, 3);              // doesn't compile
+    Shape*  const p = new Triangle(2, 3, 4);
+    p->move(5, 6);
+    assert(p->area() == 3.14 * 4 * 4);              // mark area virtual in shape
+//  p->radius();                                    // doesn't compile
+    if (Circle* const q = dynamic_cast<Circle*>(p))
+        assert(q->radius() == 4);
+    try {
+        Circle& r = dynamic_cast<Circle&>(*p);
+        assert(r.radius() == 4);}
+    catch (const bad_cast& e) {
+        assert(false);}
+    delete p;}
+#endif
+
+class Shape {
+	friend ... operator ==
+		call equals
+
+	protected:
+		virtual ... equals (const Shape& rhs)
+
+class Circle {
+	// I no longer need ==
+
+	protected
+		...equals (const Shape& rhs)
+			// needs to call Shape::equals
+			// needs dynamic_cast
+	}
+
+#ifdef TEST9
+void test9 () {
+    const Shape* const p = new Circle(2, 3, 4);
+          Shape* const q = new Circle(2, 3, 5);
+    assert(*p != *q);
+//  *q = *p;                                                    // illdefined
+    if (const Circle* const r = dynamic_cast<const Circle*>(p))
+        if (Circle* const s = dynamic_cast<Circle*>(q)) {
+            assert(*r != *s);
+            *s = *r;
+            assert(*r == *s);}
+    delete p;
+    delete q;}
+#endif
+
+#ifdef TEST10
+void test10 () {
+    istringstream sin("5 6 7");
+    Shape* p = new Circle(2, 3, 4);
+    Shape* q = new Circle(5, 6, 7);
+    sin >> *p;
+    assert(*p == *q);}
+#endif
+
+#ifdef TEST11
+void test11 () {
+    ostringstream sout;
+    Shape* p = new Circle(2, 3, 4);
+    sout << *p;
+    assert(sout.str() == "2 3 4");}
+#endif
